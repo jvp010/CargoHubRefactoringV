f523deaf4a9c49ca70f47a3378deae339967e8c3
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-11-26 18:22:09 +0100

    made the models ready to be put in DB

--- DBcontext.cs
+++ DBcontext.cs
@@ -1,15 +1,23 @@
 using Microsoft.EntityFrameworkCore;
-    public class RandomObject
-    {
-         public int Id { get; set; }
-        public string Name { get; set; }
-    }
-
     public class ModelContext : DbContext
     {
         // DbSets for your entities
 
-        public DbSet<RandomObject> RandomObject { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Inventory> inventories {get;set;}
+        public DbSet<ItemGroup> ItemGroups {get;set;}
+        public DbSet<ItemLine> itemLines {get;set;}
+        public DbSet<ItemType> itemTypes {get;set;}
+        public DbSet<Item> items {get;set;}
+        public DbSet<Location> locations {get;set;}
+        public DbSet<Order> orders {get;set;}
+        public DbSet<OrderItem> orderItems {get;set;}
+        public DbSet<Shipment> shipments {get;set;}
+        public DbSet<ShipmentItem> shipmentItems {get;set;}
+        public DbSet<Supplier> suppliers {get;set;}
+        public DbSet<Transfer> transfers {get;set;}
+        public DbSet<TransferItem> transferItems {get;set;}
+        public DbSet<Warehouse> warehouses {get;set;}
 
 
 
