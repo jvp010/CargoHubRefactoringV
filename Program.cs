f060d640b3bbe991beac121b20b6caf7fb9ccc54
Hasan Cena <1051690@hr.nl>
2024-12-02 15:36:03 +0100

    Paar Stukjes van mijn deel toegevoegd die blijkbaar al gemaatk zijn

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 /////////////////////////////////////////////////////
-builder.Services.AddDbContext<ModelContext>(options => 
+builder.Services.AddDbContext<ModelContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DBConnection"))
                        .EnableSensitiveDataLogging());  // Enable detailed logging
 // postgres DB
@@ -29,44 +29,44 @@ app.UseHttpsRedirection();
 
 using (var scope = app.Services.CreateScope())
 {
-  Console.WriteLine("enter in yes to load the jsons");
-  string answer = Console.ReadLine();
-  
-  if(answer == "yes")
-  {
+    Console.WriteLine("enter in yes to load the jsons");
+    string answer = Console.ReadLine();
+
+    if (answer == "yes")
+    {
         var context = scope.ServiceProvider.GetRequiredService<ModelContext>();
 
         string jsonClient = File.ReadAllText("data/clients.json");
         List<Client> clients = JsonSerializer.Deserialize<List<Client>>(jsonClient);
         context.Clients.AddRange(clients); // succes
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
-        string jsonItemLines= File.ReadAllText("data/item_lines.json");                                           
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
+        string jsonItemLines = File.ReadAllText("data/item_lines.json");
         List<ItemLine> ItemLines = JsonSerializer.Deserialize<List<ItemLine>>(jsonItemLines);
         List<ItemLine> newItemLines = [];
         foreach (var item in ItemLines)
         {
-            newItemLines.Add(new ItemLine{id = 0, name = item.name , description = item.description , created_at = item.created_at , updated_at = item.updated_at});
-        } 
+            newItemLines.Add(new ItemLine { id = 0, name = item.name, description = item.description, created_at = item.created_at, updated_at = item.updated_at });
+        }
         context.ItemLines.AddRange(newItemLines);
 
-        string jsonItemGroups= File.ReadAllText("data/item_groups.json");                                           
-        List<ItemGroup> ItemGroups = JsonSerializer.Deserialize<List<ItemGroup>>(jsonItemGroups);    
+        string jsonItemGroups = File.ReadAllText("data/item_groups.json");
+        List<ItemGroup> ItemGroups = JsonSerializer.Deserialize<List<ItemGroup>>(jsonItemGroups);
         List<ItemGroup> newItemGroup = [];
         foreach (var item in ItemGroups)
         {
-            newItemGroup.Add(new ItemGroup{id = 0, name = item.name , description = item.description , created_at = item.created_at , updated_at = item.updated_at});
-        } 
+            newItemGroup.Add(new ItemGroup { id = 0, name = item.name, description = item.description, created_at = item.created_at, updated_at = item.updated_at });
+        }
         context.ItemGroups.AddRange(newItemGroup);
 
-        string jsonItemTypes= File.ReadAllText("data/item_types.json");                                           
-        List<ItemType> ItemTypes = JsonSerializer.Deserialize<List<ItemType>>(jsonItemTypes);    
+        string jsonItemTypes = File.ReadAllText("data/item_types.json");
+        List<ItemType> ItemTypes = JsonSerializer.Deserialize<List<ItemType>>(jsonItemTypes);
         List<ItemType> newItemType = [];
         foreach (var item in ItemTypes)
         {
-            newItemType.Add(new ItemType{id = 0, name = item.name , description = item.description , created_at = item.created_at , updated_at = item.updated_at});
-        } 
-        context.ItemTypes.AddRange(newItemType);  
+            newItemType.Add(new ItemType { id = 0, name = item.name, description = item.description, created_at = item.created_at, updated_at = item.updated_at });
+        }
+        context.ItemTypes.AddRange(newItemType);  //
 
 
         string jsonSuppliers = File.ReadAllText("data/suppliers.json");
@@ -75,7 +75,7 @@ using (var scope = app.Services.CreateScope())
 
 
         context.SaveChanges();    // succes
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         string jsonItem = File.ReadAllText("data/items.json");
         List<Item> items = JsonSerializer.Deserialize<List<Item>>(jsonItem);
@@ -88,7 +88,7 @@ using (var scope = app.Services.CreateScope())
         context.Shipments.AddRange(Shipments);
         context.SaveChanges();    // succes
 
-        
+
         string jsonOrder = File.ReadAllText("data/orders.json");
         List<Order> Orders = JsonSerializer.Deserialize<List<Order>>(jsonOrder);
         List<Order> newOrders = new List<Order>();
@@ -128,7 +128,7 @@ using (var scope = app.Services.CreateScope())
 
 
 
-        
+
         string jsonWarehouse = File.ReadAllText("data/warehouses.json");
         List<Warehouse> Warehouses = JsonSerializer.Deserialize<List<Warehouse>>(jsonWarehouse);
         context.Warehouses.AddRange(Warehouses); // succes
@@ -139,26 +139,32 @@ using (var scope = app.Services.CreateScope())
         List<Location> locations = JsonSerializer.Deserialize<List<Location>>(jsonLocation);
         context.Locations.AddRange(locations); // succes
 
-        
+        //string jsonLocation = File.ReadAllText("data/locations.json");
+        //List<Location> locations = JsonSerializer.Deserialize<List<Location>>(jsonLocation);
+        //context.Transfers.AddRange(locations.json)
+
+
+
         string jsonTransfer = File.ReadAllText("data/transfers.json");
         List<Transfer> Transfers = JsonSerializer.Deserialize<List<Transfer>>(jsonTransfer);
         context.Transfers.AddRange(Transfers); // succes
 
         context.SaveChanges();
-  }
-  
+    }
+
 
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.Run();
 
-static bool DuplicateCheck(List<ItemGroup> itemGroups){
+static bool DuplicateCheck(List<ItemGroup> itemGroups)
+{
     List<int> Ids = [];
 
     foreach (var item in itemGroups)
     {
-        if(Ids.Contains(item.id)) return true;
+        if (Ids.Contains(item.id)) return true;
         Ids.Add(item.id);
     }
     return false;
