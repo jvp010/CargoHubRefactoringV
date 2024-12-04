7431c827392d7649e647169268f2545d111b1bc9
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-12-04 10:00:23 +0100

    Many-To-Many relation erin gezet van LocationInventory

--- Models/Common/Location.cs
+++ Models/Common/Location.cs
@@ -1,16 +1,16 @@
-// public class Location : BaseEntity
-// {
-//     public int warehouse_id { get; set; }
-//     public string code { get; set; }
-//     public string name { get; set; }
-//     public ICollection<Inventory> inventories { get; set; }
-// }
-
 public class Location : BaseEntity
 {
     public int warehouse_id { get; set; }
     public string code { get; set; }
     public string name { get; set; }
-
-
+    public ICollection<Inventory> inventories { get; set; }
 }
+
+// public class Location : BaseEntity
+// {
+//     public int warehouse_id { get; set; }
+//     public string code { get; set; }
+//     public string name { get; set; }
+
+
+// }
