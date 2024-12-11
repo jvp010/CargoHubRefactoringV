0c179f03dd1f47b3a9763b66713e0f709b07c1b0
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-12-11 15:44:02 +0100

    .

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace MyApp.Tests
+{
+    public class SimpleMathTests
+    {
+        [Fact]
+        public void Addition_TwoPlusZero_IsTwo()
+        {
+            int a = 1;
+            int b = 1;
+            Assert.Equal(2, a + b);
+        }
+    }
+}
