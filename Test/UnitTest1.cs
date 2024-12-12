71a72239c8da8ed2039b9947d29c69d354c1cba8
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-12-12 17:02:57 +0100

    artifact added

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace MyApp.Tests
         public void Addition_TwoPlusZero_IsTwo()
         {
             int a = 1;
-            int b = 2;
+            int b = 1;
             Assert.Equal(2, a + b);
         }
     }
