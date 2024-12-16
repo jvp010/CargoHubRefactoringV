6bc1e778337b42b303d9af95cc81e2ed74319ac3
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-12-16 16:44:01 +0100

    i forgot to make the prints with Console.WriteLine()

--- UnitTest/SimpleTest.cs
+++ UnitTest/SimpleTest.cs
@@ -382,8 +382,8 @@ public class CRUDTest
         var UpdatedInventory = InventoryService.Get(1);
 
         // Then
-        Assert.Single($"Now: {DateTime.UtcNow}");
-        Assert.Single($"UpdatedAt: {UpdatedInventory.UpdatedAt}");
+        Console.WriteLine($"Now: {DateTime.UtcNow}");
+        Console.WriteLine($"UpdatedAt: {UpdatedInventory.UpdatedAt}");
 
         Assert.True(DateTime.Parse(UpdatedInventory.UpdatedAt) > now);
         Assert.True((DateTime.UtcNow - DateTime.Parse(UpdatedInventory.UpdatedAt)).Milliseconds < 500);
